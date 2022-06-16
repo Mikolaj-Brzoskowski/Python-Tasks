6094611a91a902ce1fa1c860977897c0bbd5bd77
Mikołaj Brzoskowski <mikolaj.brzoskowski2000@gmail.com>
2022-06-16 17:32:15 +0200

    project report done

--- Projekt2/hamilton.py
+++ Projekt2/hamilton.py
@@ -14,13 +14,13 @@ import json
 script = """"""
 names = []
 
-with open('IO\Projekt2\script.json') as json_file:
+with open('Inteligencja Obliczeniowa\Projekt2\script.json') as json_file:
     script_dict = json.load(json_file)
 
     
 script_no_names=[]
 
-with open('IO\Projekt2\Hamilton.txt') as f:
+with open('Inteligencja Obliczeniowa\Projekt2\Hamilton.txt') as f:
     script = f.read()
 
 tokenized_text=sent_tokenize(script)
@@ -70,43 +70,46 @@ for w in filtered_word:
 #             script_dict[name].append(word)
             
 #ocena emocji
-
-# sia = SentimentIntensityAnalyzer()
-# for key in script_dict:
-#     name_neg = 0
-#     name_neu = 0
-#     name_pos = 0
-#     name_comp = 0
-#     name_angry = 0
-#     name_fear = 0
-#     name_happy = 0
-#     name_sad = 0
-#     name_surprise = 0
-#     for word in script_dict[key]:
-#         #zliczanie negatywnych / pozytywnych emocji
-#         name_pos += sia.polarity_scores(word)["pos"]
-#         name_neu += sia.polarity_scores(word)["neu"]
-#         name_neg += sia.polarity_scores(word)["neg"]
-#         name_comp += sia.polarity_scores(word)["compound"]
-#         #zliczanie kontretnych uczuć
-#         name_angry += te.get_emotion(word)["Angry"]
-#         name_fear += te.get_emotion(word)["Fear"]
-#         name_happy += te.get_emotion(word)["Happy"]
-#         name_sad += te.get_emotion(word)["Sad"]
-#         name_surprise += te.get_emotion(word)["Surprise"]
-#     word_count = len(script_dict[key])
-#     #emocje
-#     average_neg = name_neg / word_count
-#     average_neu = name_neu / word_count
-#     average_pos = name_pos / word_count
-#     average_comp = name_comp / word_count
-#     #uczucia
-#     average_angry = name_angry / word_count
-#     average_fear = name_fear / word_count
-#     average_happy = name_happy / word_count
-#     average_sad = name_sad / word_count
-#     average_surprise = name_surprise / word_count
-#     print("Average neg/pos scores for " + key + ", neg: " + str(average_neg) + ", neu: " + 
-#           str(average_neu) + ", pos: " + str(average_pos) + ", compound: " + str(average_comp))
-#     print("Average emotion scores for " + key + ", angry: " + str(average_angry) + ", fear: " + 
-#           str(average_fear) + ", happy: " + str(average_happy) + ", sad: " + str(average_sad) + ", surprise: " + str(average_surprise))
+nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+for key in script_dict:
+    name_neg = 0
+    name_neu = 0
+    name_pos = 0
+    name_comp = 0
+    name_angry = 0
+    name_fear = 0
+    name_happy = 0
+    name_sad = 0
+    name_surprise = 0
+    words_with_emotion_count = 0
+    for word in script_dict[key]:
+        #zliczanie negatywnych / pozytywnych emocji
+        name_pos += sia.polarity_scores(word)["pos"]
+        name_neu += sia.polarity_scores(word)["neu"]
+        name_neg += sia.polarity_scores(word)["neg"]
+        name_comp += sia.polarity_scores(word)["compound"]
+        #zliczanie kontretnych uczuć
+        if te.get_emotion(word)["Angry"] != 0 or te.get_emotion(word)["Fear"] != 0 or te.get_emotion(word)["Happy"] != 0 or te.get_emotion(word)["Sad"] != 0 or te.get_emotion(word)["Surprise"] != 0:
+            name_angry += te.get_emotion(word)["Angry"]
+            name_fear += te.get_emotion(word)["Fear"]
+            name_happy += te.get_emotion(word)["Happy"]
+            name_sad += te.get_emotion(word)["Sad"]
+            name_surprise += te.get_emotion(word)["Surprise"]
+            words_with_emotion_count += 1
+    word_count = len(script_dict[key])
+    #emocje
+    average_neg = name_neg / word_count
+    average_neu = name_neu / word_count
+    average_pos = name_pos / word_count
+    average_comp = name_comp / word_count
+    #uczucia
+    average_angry = name_angry / words_with_emotion_count
+    average_fear = name_fear / words_with_emotion_count
+    average_happy = name_happy / words_with_emotion_count
+    average_sad = name_sad / words_with_emotion_count
+    average_surprise = name_surprise / words_with_emotion_count
+    print("Average neg/pos scores for " + key + ", neg: " + str(average_neg) + ", neu: " + 
+          str(average_neu) + ", pos: " + str(average_pos) + ", compound: " + str(average_comp))
+    print("Average emotion scores for " + key + ", angry: " + str(average_angry) + ", fear: " + 
+          str(average_fear) + ", happy: " + str(average_happy) + ", sad: " + str(average_sad) + ", surprise: " + str(average_surprise))
